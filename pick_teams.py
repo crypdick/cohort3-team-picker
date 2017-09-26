bf698c33b00a01ae0743e5091a360161db9bb85e
Richard Decal <crypdick@users.noreply.github.com>
2017-09-26 10:22:51 -0400

    init with names

---
+++ pick_teams.py
@@ -0,0 +1,5 @@
+
+
+cohort_3_list = ["Seth", "Jeremy", "Lydia", "Saad", "Kevin", "Richard", "Xiaotai", "David", "Fanny", "Lillianne", "Stephanie", "Beau", "Mach", "Noah", "Shane", "Joe", "Michael", "Roger", "Adrianna", "Jackie"]
+
+previous_paired = 
